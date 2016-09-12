0c7807c83c4e70160de1ae0b554d813c09382503
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-09-12 02:25:21 -0500

    Started on JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+// var newTrainName = $('#addTrainName').val().trim();
+// var newTrainDestination = $('#addTrainDestination').val().trim();
+// var newFirstTrainTime = $('#addFirstTrainTime').val().trim();
+// var newTrainFrequency= $('#addTrainFrequency').val().trim();
+
+
+$('#submit').on('click', function(){
+	var newTrainName = $('#addTrainName').val().trim();
+	var newTrainDestination = $('#addTrainDestination').val().trim();
+	var newFirstTrainTime = $('#addFirstTrainTime').val().trim();
+	var newTrainFrequency= $('#addTrainFrequency').val().trim();
+	console.log(newTrainName);
+	console.log(newTrainDestination);
+	console.log(newFirstTrainTime);
+	console.log(newTrainFrequency);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //End jQuery
+
+
+
+
+
+
+
+
+
+
