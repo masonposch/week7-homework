6adbb06fc4cc18e5793b46b6f864e4f52d12fa88
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-09-13 00:41:46 -0500

    figuring out the logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
 
-// var newTrainName = $('#addTrainName').val().trim();
-// var newTrainDestination = $('#addTrainDestination').val().trim();
-// var newFirstTrainTime = $('#addFirstTrainTime').val().trim();
-// var newTrainFrequency= $('#addTrainFrequency').val().trim();
+var newTrainName;
+var newTrainDestination;
+var newFirstTrainTime;
+var newTrainFrequency;
+
+var nextTrainArrival;
+var minutesAway;
 
 
 $('#submit').on('click', function(){
-	var newTrainName = $('#addTrainName').val().trim();
-	var newTrainDestination = $('#addTrainDestination').val().trim();
-	var newFirstTrainTime = $('#addFirstTrainTime').val().trim();
-	var newTrainFrequency= $('#addTrainFrequency').val().trim();
+	newTrainName = $('#addTrainName').val().trim();
+	newTrainDestination = $('#addTrainDestination').val().trim();
+	newFirstTrainTime = $('#addFirstTrainTime').val().trim();
+	var trainTimeSplit = newFirstTrainTime.split(':');
+	var trainTimeRaw = trainTimeSplit[0] + trainTimeSplit[1];
+	newTrainFrequency= parseInt($('#addTrainFrequency').val().trim());
+
+	var currentTime = new Date(new Date().getTime()).toLocaleTimeString(); 
+
+
 	console.log(newTrainName);
 	console.log(newTrainDestination);
-	console.log(newFirstTrainTime);
+	console.log(trainTimeRaw);
 	console.log(newTrainFrequency);
+	console.log(currentTime);
 });
 
 
